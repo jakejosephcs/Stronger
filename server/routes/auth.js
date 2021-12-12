956cace71c5af5bedb7de759bac789fac09c7ba0
Jake Joseph <jakejosephcs@gmail.com>
2021-12-12 14:00:04 -0500

    Add JWT and dotenv file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
+require("dotenv").config();
 const { signupValidation } = require("../validation");
 
 const router = express.Router();
@@ -15,14 +17,17 @@ router.post("/signup", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    res.json({ email, password: hashedPassword });
+
+    // JWT
+    const token = await JWT.sign({ email }, process.env.SECRET_KEY);
+
+    res.json({ email, password: hashedPassword, token });
   } catch (error) {
     res.status(500).json(error);
   }
 
   // # TO DO
   // - Check uniqueness of email when implemented the database
-  // - Implement JWT
 });
 
 module.exports = router;
