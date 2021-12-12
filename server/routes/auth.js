09040fbf37d1905a885ac6e6dba75cb391cc72b4
Jake Joseph <jakejosephcs@gmail.com>
2021-12-11 21:43:12 -0500

    Implement password hashing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+
 const { signupValidation } = require("../validation");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
+  // Input validation using Joi
   const { error } = signupValidation(req.body);
-
   if (error) return res.status(400).send(error.details[0].message);
 
   const { email, password } = req.body;
 
-  res.json({ email, password });
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    res.json({ email, password: hashedPassword });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+  // # TO DO
+  // - Check uniqueness of email when implemented the database
+  // - Implement JWT
 });
 
 module.exports = router;
