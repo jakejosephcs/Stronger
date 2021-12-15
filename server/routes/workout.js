f2c366c71c9811db5b62b1c27f423d9077c1649f
Jake Joseph <jakejosephcs@gmail.com>
2021-12-14 21:20:14 -0500

    Add exercise to workout

--- server/routes/workout.js
+++ server/routes/workout.js
@@ -5,6 +5,7 @@
 const express = require("express");
 
 const Workout = require("../models/Workout");
+const Exercise = require("../models/Exercise");
 const User = require("../models/User");
 
 const router = express.Router();
@@ -50,6 +51,41 @@ router.get("/:id", async (req, res) => {
     const workout = await Workout.findById(workoutId);
     if (!workout)
       return res.status(404).json({ error: "Workout does not exist" });
+    res.json(workout);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Add an exercise to a workout
+router.post("/exercise/", async (req, res) => {
+  const { exerciseId, workoutId, sets, reps, weights } = req.body;
+
+  // sets: Number
+  // reps: [Number, Number, ... , Number]
+  // weight: [Number, Number, ... , Number]
+
+  try {
+    const exercise = await Exercise.findById(exerciseId);
+    const workout = await Workout.findById(workoutId);
+
+    workout.exercises.push(exercise);
+
+    await workout.save();
+
+    const newExercise = await Workout.findOne({
+      "exercise.id": exerciseId,
+    });
+
+    for (let i = 0; i < sets; i++) {
+      newExercise.exercises[0].sets.push({
+        reps: reps[i],
+        weight: weights[i],
+      });
+    }
+
+    await newExercise.save();
+
     res.json(workout);
   } catch (error) {
     res.status(500).json(error);
