f31e22515828ed65daa15b5e7155629e49043597
Jake Joseph <jakejosephcs@gmail.com>
2021-12-14 19:04:00 -0500

    Add get all workouts and get one workout routes

--- server/routes/workout.js
+++ server/routes/workout.js
@@ -1,3 +1,7 @@
+// # TO DO:
+// - Add server side validation
+// - Add error handling
+
 const express = require("express");
 
 const Workout = require("../models/Workout");
@@ -23,6 +27,30 @@ router.post("/", async (req, res) => {
     await user.save();
 
     res.json(savedWorkout);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get all workouts
+router.get("/", async (req, res) => {
+  try {
+    const allWorkouts = await Workout.find();
+    res.json(allWorkouts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get a single workout
+router.get("/:id", async (req, res) => {
+  const workoutId = req.params.id;
+
+  try {
+    const workout = await Workout.findById(workoutId);
+    if (!workout)
+      return res.status(404).json({ error: "Workout does not exist" });
+    res.json(workout);
   } catch (error) {
     res.status(500).json(error);
   }
