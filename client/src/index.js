f2593dce6a511583d27c11a4dcc082201cfafd1f
Jake Joseph <jakejosephcs@gmail.com>
2021-12-16 20:48:42 -0500

    Add react router dom and tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Registration from "./Pages/Registration";
+import Login from "./Pages/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
