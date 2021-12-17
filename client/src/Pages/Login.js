264d4380028b435bb2b2a92a2bd5739150de5cfe
Jake Joseph <jakejosephcs@gmail.com>
2021-12-16 20:37:57 -0500

    Create login and registration pages:

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import LoginForum from "../Components/LoginForum";
+
+function Login() {
+  return (
+    <div>
+      <Header />
+      <LoginForum />
+      <Footer />
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/Pages/Registration.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import RegistrationForum from "../Components/RegistrationForum";
+
+function Registration() {
+  return (
+    <div>
+      <Header />
+      <RegistrationForum />
+      <Footer />
+    </div>
+  );
+}
+
+export default Registration;
