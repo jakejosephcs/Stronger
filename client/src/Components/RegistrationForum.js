4c3c82169df980cc3f26a0b08d80c2b508acefe7
Jake Joseph <jakejosephcs@gmail.com>
2021-12-16 20:36:23 -0500

    Add registration logic

--- client/src/Components/RegistrationForum.js
+++ client/src/Components/RegistrationForum.js
@@ -1,43 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function RegistrationForum() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleUserChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/auth/signup", {
+        email: user.email,
+        password: user.password,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div class="w-full max-w-xs">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
+    <div className="w-full max-w-xs">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleFormSubmit}
+      >
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
           >
-            Username
+            Email
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="example@mail.com"
+            name="email"
+            value={user.email}
+            onChange={handleUserChange}
           />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
           >
             Password
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
+            name="password"
+            value={user.password}
+            onChange={handleUserChange}
           />
-          {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+          {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
         </div>
-        <div class="flex items-center justify-between">
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            Sign Up
-          </button>
+        <div className="flex items-center justify-between">
+          <input
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:cursor-pointer"
+            type="submit"
+            value="Sign Up"
+          />
         </div>
       </form>
     </div>
