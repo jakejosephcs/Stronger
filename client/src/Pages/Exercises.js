a2ef674700ec705244298479fe79e19cb37108c6
Jake Joseph <jakejosephcs@gmail.com>
2021-12-17 19:17:47 -0500

    Create preliminary exercise page and logic

---
+++ client/src/Pages/Exercises.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Exercises from "../Components/Exercises";
+
+function Exercise() {
+  return (
+    <div>
+      <Header />
+      <Exercises />
+      <Footer />
+    </div>
+  );
+}
+
+export default Exercise;
