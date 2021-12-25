8b75744d67558cb26b851eda515a8841449236b7
Jake Joseph <jakejosephcs@gmail.com>
2021-12-25 17:13:03 -0500

    Add preliminary loading state

--- client/src/Pages/Exercises.js
+++ client/src/Pages/Exercises.js
@@ -11,14 +11,17 @@ import DeleteIcon from "../Assests/DeleteIcon";
 function Exercise() {
   const [exercises, setExercises] = useState([]);
   const [userId, setUserId] = useState("61c7934ca2da76efc8b719a6");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get("http://localhost:5000/exercises/").then((res) => {
       const allExercises = res.data;
       const userExercises = allExercises.filter(
         (exercise) => exercise.userId === userId
       );
       setExercises(userExercises);
+      setIsLoading(false);
     });
   }, []);
 
@@ -38,6 +41,22 @@ function Exercise() {
       .catch((e) => console.log(e));
   };
 
+  const displayExercises = () => {
+    return exercises.map((exercise) => (
+      <div
+        key={exercise._id}
+        className="flex justify-between w-56 bg-slate-200 py-2 px-3 rounded mb-4"
+      >
+        <h3 className="truncate text-ellipsis overflow-hidden">
+          {exercise.name}
+        </h3>
+        <button onClick={() => handleDeleteExercise(exercise._id)}>
+          <DeleteIcon />
+        </button>
+      </div>
+    ));
+  };
+
   return (
     <div className="flex flex-col max-w-sm mx-auto items-center">
       <div className="flex bg-slate-200 py-2 px-3 rounded mb-12">
@@ -46,19 +65,7 @@ function Exercise() {
       </div>
       <div className="flex flex-col items-center">
         <h1 className="text-2xl font-bold mb-4">All exercises</h1>
-        {exercises.map((exercise) => (
-          <div
-            key={exercise._id}
-            className="flex justify-between w-56 bg-slate-200 py-2 px-3 rounded mb-4"
-          >
-            <h3 className="truncate text-ellipsis overflow-hidden">
-              {exercise.name}
-            </h3>
-            <button onClick={() => handleDeleteExercise(exercise._id)}>
-              <DeleteIcon />
-            </button>
-          </div>
-        ))}
+        {isLoading ? <p>Loading...</p> : displayExercises()}
       </div>
     </div>
   );
