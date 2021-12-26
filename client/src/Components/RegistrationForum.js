03b69821babba8dfed21753dd1dc8ed734500503
Jake Joseph <jakejosephcs@gmail.com>
2021-12-25 19:34:57 -0500

    Add redirect after successful sign up

--- client/src/Components/RegistrationForum.js
+++ client/src/Components/RegistrationForum.js
@@ -4,12 +4,14 @@
 
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function RegistrationForum() {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
+  const navigate = useNavigate();
 
   const handleUserChange = (e) => {
     setUser({
@@ -25,61 +27,59 @@ export default function RegistrationForum() {
         email: user.email,
         password: user.password,
       })
-      .then((res) => console.log(res))
+      .then((res) => navigate("/"))
       .catch((err) => console.log(err));
   };
 
   return (
-    <div className="w-full max-w-xs">
-      <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleFormSubmit}
-      >
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
-            id="email"
-            type="email"
-            placeholder="example@mail.com"
-            name="email"
-            value={user.email}
-            onChange={handleUserChange}
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
-            id="password"
-            type="password"
-            placeholder="******************"
-            name="password"
-            value={user.password}
-            onChange={handleUserChange}
-            required
-            minLength="6"
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <input
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:cursor-pointer"
-            type="submit"
-            value="Sign Up"
-          />
-        </div>
-      </form>
-    </div>
+    <form
+      className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mx-3"
+      onSubmit={handleFormSubmit}
+    >
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
+          id="email"
+          type="email"
+          placeholder="example@mail.com"
+          name="email"
+          value={user.email}
+          onChange={handleUserChange}
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
+          id="password"
+          type="password"
+          placeholder="******************"
+          name="password"
+          value={user.password}
+          onChange={handleUserChange}
+          required
+          minLength="6"
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <input
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:cursor-pointer"
+          type="submit"
+          value="Sign Up"
+        />
+      </div>
+    </form>
   );
 }
