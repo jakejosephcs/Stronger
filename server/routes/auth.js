ef6998966db085eb2a55996a0a6582cdea3f6e5a
Jake Joseph <jakejosephcs@gmail.com>
2021-12-27 22:55:11 -0500

    Modify error messages for sign up route and add try catch for login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,14 +17,13 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   // Input validation using Joi
   const { error } = authValidation(req.body);
-  if (error) return res.status(400).send({ error: error.details[0].message });
+  if (error) return res.status(400).send(error.details[0].message);
 
   const { email, password } = req.body;
 
   // Checking if the user's email is already inside the database
   const emailExists = await User.findOne({ email });
-  if (emailExists)
-    return res.status(400).send({ error: "Email already exists" });
+  if (emailExists) return res.status(400).send("Email already exists");
 
   try {
     // Hashing the password
@@ -42,7 +41,7 @@ router.post("/signup", async (req, res) => {
 
     res.json(user);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).send(error);
   }
 });
 
@@ -54,19 +53,23 @@ router.post("/login", async (req, res) => {
 
   const { email, password } = req.body;
 
-  // Get the user from the database
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).send("Invalid email or password");
+  try {
+    // Get the user from the database
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send("Invalid email or password");
 
-  // Checking if the password provided is correct
-  const isUserPasswordCorrect = await bcrypt.compare(password, user.password);
-  if (!isUserPasswordCorrect)
-    return res.status(400).send("Invalid email or password");
+    // Checking if the password provided is correct
+    const isUserPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isUserPasswordCorrect)
+      return res.status(400).send("Invalid email or password");
 
-  // Create and assign a JWT token
-  const token = JWT.sign({ _id: user._id }, process.env.SECRET_KEY);
+    // Create and assign a JWT token
+    const token = JWT.sign({ _id: user._id }, process.env.SECRET_KEY);
 
-  res.json({ token });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
