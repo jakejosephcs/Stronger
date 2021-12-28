d603f3c5372cd882d7fb500dd0e114e8b82b0580
Jake Joseph <jakejosephcs@gmail.com>
2021-12-27 19:24:24 -0500

    Add header and subtitle

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,14 +1,17 @@
-import React from "react";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
 import LoginForum from "../Components/LoginForum";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <Header />
+    <div className="flex flex-col max-w-sm mx-auto items-center">
+      <h1 className="font-bold text-2xl mb-2">Sign into your account</h1>
+      <span className="mb-5">
+        New to Stronger?{" "}
+        <Link to="/registration" className="text-blue-500 font-bold">
+          Sign up
+        </Link>
+      </span>
       <LoginForum />
-      <Footer />
     </div>
   );
 }
