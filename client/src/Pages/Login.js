c1d5b29650430142db7baad7b3a06604bf8e3194
Jake Joseph <jakejosephcs@gmail.com>
2021-12-28 14:14:56 -0500

    Add reusable auth header component

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,16 +1,11 @@
 import LoginForum from "../Components/LoginForum";
 import { Link } from "react-router-dom";
+import AuthHeader from "../Components/AuthHeader";
 
 function Login() {
   return (
     <div className="flex flex-col max-w-sm mx-auto items-center">
-      <h1 className="font-bold text-2xl mb-2">Sign into your account</h1>
-      <span className="mb-5">
-        New to Stronger?{" "}
-        <Link to="/registration" className="text-blue-500 font-bold">
-          Sign up
-        </Link>
-      </span>
+      <AuthHeader isLoginHeader={true} />
       <LoginForum />
     </div>
   );
--- client/src/Pages/Registration.js
+++ client/src/Pages/Registration.js
@@ -1,16 +1,11 @@
 import RegistrationForum from "../Components/RegistrationForum";
 import { Link } from "react-router-dom";
+import AuthHeader from "../Components/AuthHeader";
 
 function Registration() {
   return (
     <div className="flex flex-col max-w-sm mx-auto items-center">
-      <h1 className="font-bold text-2xl mb-2">Create your account</h1>
-      <span className="mb-5">
-        Already registered?{" "}
-        <Link to="/login" className="text-blue-500 font-bold">
-          Sign in
-        </Link>
-      </span>{" "}
+      <AuthHeader isLoginHeader={false} />
       <RegistrationForum />
     </div>
   );
