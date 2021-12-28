6b01d8b1cc27e20f2010c96d31ac7aececd3e57d
Jake Joseph <jakejosephcs@gmail.com>
2021-12-28 14:09:41 -0500

    Add header and subtitle

--- client/src/Pages/Registration.js
+++ client/src/Pages/Registration.js
@@ -1,14 +1,17 @@
-import React from "react";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
 import RegistrationForum from "../Components/RegistrationForum";
+import { Link } from "react-router-dom";
 
 function Registration() {
   return (
-    <div>
-      <Header />
+    <div className="flex flex-col max-w-sm mx-auto items-center">
+      <h1 className="font-bold text-2xl mb-2">Create your account</h1>
+      <span className="mb-5">
+        Already registered?{" "}
+        <Link to="/login" className="text-blue-500 font-bold">
+          Sign in
+        </Link>
+      </span>{" "}
       <RegistrationForum />
-      <Footer />
     </div>
   );
 }
