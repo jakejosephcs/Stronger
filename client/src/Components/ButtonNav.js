d8a61302321eefe6d82396e0a05ec4379b03a642
Jake Joseph <jakejosephcs@gmail.com>
2022-01-02 22:04:38 -0500

    Update to move event onto button onClick event

--- client/src/Components/ButtonNav.js
+++ client/src/Components/ButtonNav.js
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 import LinkIcon from "../Assests/LinkIcon";
 
-function ButtonNav({ navigateTo, buttonText }) {
+function ButtonNav({ navigateTo, buttonText, onClick }) {
+  const navigate = useNavigate();
   return (
     <div className="flex bg-slate-200 py-2 px-3 rounded mb-4">
       <LinkIcon />
-      <Link to={navigateTo}>
-        <button className="ml-1">{buttonText}</button>
-      </Link>
+      <button
+        className="ml-1"
+        onClick={navigateTo ? () => navigate(navigateTo) : onClick}
+      >
+        {buttonText}
+      </button>
     </div>
   );
 }
