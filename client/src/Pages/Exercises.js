67e2affad0f576aa93e4a49acd0d8d31db6d0e01
Jake Joseph <jakejosephcs@gmail.com>
2022-01-08 19:42:41 -0500

    Update UI state and minor changes

--- client/src/Pages/Exercises.js
+++ client/src/Pages/Exercises.js
@@ -1,29 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import DeleteIcon from "../Assests/DeleteIcon";
 import CloseIcon from "../Assests/CloseIcon";
 import ButtonNav from "../Components/ButtonNav";
 import ExerciseCard from "../Components/ExerciseCard";
+import Container from "../Components/Container";
+import LinkIcon from "../Assests/LinkIcon";
+import UICard from "../Components/UICard";
+import ButtonPrimary from "../Components/ButtonPrimary";
 
 function Exercise() {
   const [exercises, setExercises] = useState([]);
   const [userId, setUserId] = useState("61ca404e5a56d8505f193391");
-  const [isExercisesLoading, setIsExercisesLoading] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [newExerciseName, setNewExerciseName] = useState("");
-  const [error, setError] = useState("");
+
+  const [isExercisesLoading, setIsExercisesLoading] = useState(false);
+  const [exercisesError, setExercisesError] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [isCreateExerciseLoading, setIsCreateExerciseLoading] = useState(false);
+  const [createExerciseError, setCreateExerciseError] = useState("");
 
   useEffect(() => {
     setIsExercisesLoading(true);
-    axios.get("http://localhost:5000/exercises/").then((res) => {
-      const allExercises = res.data;
-      const userExercises = allExercises.filter(
-        (exercise) => exercise.userId === userId
-      );
-      setExercises(userExercises);
-      setIsExercisesLoading(false);
-    });
+    axios
+      .get("http://localhost:5000/exercises/")
+      .then((res) => {
+        const allExercises = res.data;
+        const userExercises = allExercises.filter(
+          (exercise) => exercise.userId === userId
+        );
+        setExercises(userExercises);
+        setIsExercisesLoading(false);
+      })
+      .catch((error) => {
+        setIsExercisesLoading(false);
+        setExercisesError(true);
+      });
   }, []);
 
   const handleDeleteExercise = (exerciseId) => {
@@ -43,8 +56,16 @@ function Exercise() {
   };
 
   const displayExercises = () => {
+    if (isExercisesLoading) {
+      return <UICard text="Loading..." />;
+    }
+
+    if (exercisesError) {
+      return <UICard text="Uh Oh! Something went wrong" />;
+    }
+
     if (exercises.length === 0) {
-      return <p className="my-6 text-slate-500 text-sm">0 exercises exist</p>;
+      return <UICard text="0 exercises exist" />;
     }
 
     return exercises.map((exercise) => (
@@ -73,7 +94,7 @@ function Exercise() {
         setNewExerciseName("");
       })
       .catch((err) => {
-        setError(err.response.data);
+        setCreateExerciseError(err.response.data);
         setIsCreateExerciseLoading(false);
         setNewExerciseName("");
       });
@@ -85,44 +106,43 @@ function Exercise() {
 
   return (
     <>
-      <div className="flex flex-col max-w-sm mx-auto items-center">
-        <ButtonNav navigateTo="/workout" buttonText="Start a new workout" />
-        <ButtonNav buttonText="Create a new exercise" onClick={toggleModal} />
+      <Container>
+        <ButtonNav navigateTo="/workout" buttonText="Start a new workout">
+          <LinkIcon />
+        </ButtonNav>
+        <ButtonNav buttonText="Create a new exercise" onClick={toggleModal}>
+          <LinkIcon />
+        </ButtonNav>
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold mb-4">All exercises</h1>
-          {isExercisesLoading ? <p>Loading...</p> : displayExercises()}
+          {displayExercises()}
         </div>
-      </div>
+      </Container>
 
       {isModalOpen && (
-        <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center">
+        <div className="bg-black bg-opacity-50 fixed inset-0 flex justify-center items-center h-screen">
           <div className="bg-gray-200 max-w-sm flex flex-col mx-auto items-center px-6 py-6 relative rounded">
-            <button
-              className="absolute top-2 right-2"
-              onClick={() => setIsModalOpen((isModalOpen) => !isModalOpen)}
-            >
+            <button className="absolute top-2 right-2" onClick={toggleModal}>
               <CloseIcon />
             </button>
             <h3 className="mt-2 font-bold mb-3">Create a new exercise</h3>
             <span className="text-xs flex justify-center mb-2 text-red-500">
-              {error}
+              {createExerciseError}
             </span>
             <input
-              className="px-3 py-1 text-sm"
+              className="px-3 py-1 text-sm mb-4"
               type="text"
               value={newExerciseName}
               onChange={(e) => {
                 setNewExerciseName(e.target.value);
-                setError("");
+                setCreateExerciseError("");
               }}
               placeholder="Exercise name"
             />
-            <button
+            <ButtonPrimary
               onClick={handleCreateNewExercise}
-              className="mt-3 bg-blue-500 py-1 px-3 rounded-full text-gray-200"
-            >
-              {isCreateExerciseLoading ? "Creating..." : "Create"}
-            </button>
+              text={isCreateExerciseLoading ? "Creating..." : "Create"}
+            />
           </div>
         </div>
       )}
