2e751f17662732ea8fd9cb5f2df5d13335989264
Jake Joseph <jakejosephcs@gmail.com>
2022-01-08 20:18:05 -0500

    Update UI state and minor changes with logic updates

--- client/src/Pages/Workout.js
+++ client/src/Pages/Workout.js
@@ -2,17 +2,16 @@ import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import DeleteIcon from "../Assests/DeleteIcon";
 import PlusIcon from "../Assests/PlusIcon";
 import XIcon from "../Assests/XIcon";
 import CloseIcon from "../Assests/CloseIcon";
-import UnCheckedCheckMark from "../Assests/UnCheckedCheckMark";
 import AddIcon from "../Assests/AddIcon";
-import { ExerciseContext } from "../Context/ExerciseContext";
 import Container from "../Components/Container";
 import WorkoutHeader from "../Components/WorkoutHeader";
 import { getCurrentDate } from "../Utils";
 import WorkoutExerciseCard from "../Components/WorkoutExerciseCard";
+import ButtonNav from "../Components/ButtonNav";
+import UICard from "../Components/UICard";
 
 function Workout() {
   const [workout, setWorkout] = useState({
@@ -22,24 +21,42 @@ function Workout() {
     date: getCurrentDate(),
     exercises: [],
   });
+
+  const [isWorkoutSubmitting, setIsWorkoutSubmitting] = useState(false);
+
+  const [error, setError] = useState("");
+
   const [exercises, setExercises] = useState([]);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [fetchedExercies, setFetchedExercises] = useState([]);
+  const [fetchedExerciesError, setFetchedExercisesError] = useState("");
+  const [isfetchedExerciesLoading, setIsfetchedExerciesLoading] =
+    useState(false);
+
   const navigate = useNavigate();
 
-  const { name } = useContext(ExerciseContext);
-
   useEffect(() => {
-    axios.get("http://localhost:5000/exercises/").then((res) => {
-      const allExercises = res.data;
-      const userExercises = allExercises.filter(
-        (exercise) => exercise.userId === workout.userId
-      );
-      setFetchedExercises(userExercises);
-    });
+    setIsfetchedExerciesLoading(true);
+    axios
+      .get("http://localhost:5000/exercises/")
+      .then((res) => {
+        const allExercises = res.data;
+        const userExercises = allExercises.filter(
+          (exercise) => exercise.userId === workout.userId
+        );
+        setFetchedExercises(userExercises);
+        setIsfetchedExerciesLoading(false);
+      })
+      .catch((err) => {
+        setIsfetchedExerciesLoading(false);
+        setFetchedExercisesError("Uh Oh! Something went wrong");
+      });
   }, []);
 
   const handleUpdateWorkout = (e) => {
+    setError("");
     setWorkout({
       ...workout,
       [e.target.name]: e.target.value,
@@ -62,6 +79,7 @@ function Workout() {
   };
 
   const handleAddExerciseToWorkout = (exercise) => {
+    setError("");
     setExercises([
       ...exercises,
       {
@@ -74,6 +92,7 @@ function Workout() {
   };
 
   const handleUpdateWeight = (event, exercise, index) => {
+    setError("");
     setExercises(
       exercises.map((ex) => {
         if (ex._id === exercise._id) {
@@ -90,6 +109,7 @@ function Workout() {
   };
 
   const handleUpdateReps = (event, exercise, index) => {
+    setError("");
     setExercises(
       exercises.map((ex) => {
         if (ex._id === exercise._id) {
@@ -106,6 +126,7 @@ function Workout() {
   };
 
   const handleFinishWorkout = () => {
+    setIsWorkoutSubmitting(true);
     const formattedExercises = exercises.map((ex) => {
       return {
         exerciseId: ex._id,
@@ -125,9 +146,65 @@ function Workout() {
       .post("http://localhost:5000/workouts/", workoutWithFormattedExercises)
       .then((res) => {
         console.log(res);
+        setIsWorkoutSubmitting(false);
         navigate("/home");
       })
-      .catch((e) => console.log(e));
+      .catch((err) => {
+        setIsWorkoutSubmitting(false);
+        setError(err.response.data);
+      });
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen((isModalOpen) => !isModalOpen);
+  };
+
+  const handleRemoveExerciseFromWorkout = (exercise) => {
+    setExercises(exercises.filter((ex) => ex._id !== exercise._id));
+  };
+
+  const renderWorkoutExerciseCard = () => {
+    if (exercises.length === 0) {
+      return <UICard text="0 exercises added" />;
+    }
+
+    return exercises.map((exercise) => (
+      <WorkoutExerciseCard
+        exercise={exercise}
+        handleAddSet={handleAddSet}
+        handleUpdateReps={handleUpdateReps}
+        handleUpdateWeight={handleUpdateWeight}
+        handleRemoveExerciseFromWorkout={handleRemoveExerciseFromWorkout}
+      />
+    ));
+  };
+
+  const renderModalExercises = () => {
+    if (isfetchedExerciesLoading) {
+      return <UICard text="Loading..." />;
+    }
+
+    if (fetchedExerciesError !== "") {
+      return <UICard text={fetchedExerciesError} />;
+    }
+
+    if (fetchedExercies.length === 0) {
+      return <UICard text="0 exercises created" />;
+    }
+
+    return fetchedExercies.map((ex) => {
+      return (
+        <div
+          key={ex._id}
+          className="flex justify-between w-56 bg-white py-2 px-3 rounded mb-3"
+        >
+          <h3 className="truncate text-ellipsis overflow-hidden">{ex.name}</h3>
+          <button onClick={() => handleAddExerciseToWorkout(ex)}>
+            <AddIcon />
+          </button>
+        </div>
+      );
+    });
   };
 
   return (
@@ -137,42 +214,38 @@ function Workout() {
           workout={workout}
           handleUpdateWorkout={handleUpdateWorkout}
           handleFinishWorkout={handleFinishWorkout}
+          isWorkoutSubmitting={isWorkoutSubmitting}
         />
-        {exercises.length !== 0 ? (
-          exercises.map((exercise) => (
-            <WorkoutExerciseCard
-              exercise={exercise}
-              handleAddSet={handleAddSet}
-              handleUpdateReps={handleUpdateReps}
-              handleUpdateWeight={handleUpdateWeight}
-            />
-          ))
-        ) : (
-          <p className="my-6 text-slate-500 text-sm">0 exercises added</p>
+        {error && (
+          <span className="mt-4 text-xs flex justify-center text-red-500">
+            {error}
+          </span>
         )}
-        <div className="flex bg-red-600 py-2 px-3 rounded mb-4 ">
-          <PlusIcon />
-          <button
-            className="text-white ml-2"
-            onClick={() => setIsModalOpen((isModalOpen) => !isModalOpen)}
+        {renderWorkoutExerciseCard()}
+        <div className="mt-5">
+          <ButtonNav
+            onClick={toggleModal}
+            buttonText="Add an exercise"
+            color="blue-600"
+            textColor="text-white"
           >
-            Add an exercise {name}
-          </button>
-        </div>
-        <div className="flex bg-blue-600 py-2 px-3 rounded mb-4">
-          <XIcon />
-          <button className="text-white ml-2" onClick={() => navigate("/home")}>
-            Cancel workout
-          </button>
+            <PlusIcon />
+          </ButtonNav>
+          <ButtonNav
+            navigateTo="/home"
+            buttonText="Cancel workout"
+            color="red-600"
+            textColor="text-white"
+          >
+            <XIcon />
+          </ButtonNav>
         </div>
       </Container>
+
       {isModalOpen && (
-        <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center">
+        <div className="bg-black bg-opacity-50 fixed inset-0 flex justify-center items-center h-screen">
           <div className="bg-gray-200 max-w-sm flex flex-col mx-auto items-center px-6 py-6 relative rounded">
-            <button
-              className="absolute top-2 right-2"
-              onClick={() => setIsModalOpen((isModalOpen) => !isModalOpen)}
-            >
+            <button className="absolute top-2 right-2" onClick={toggleModal}>
               <CloseIcon />
             </button>
             <h3 className="mt-2 font-bold mb-3">Select an exercise to add</h3>
@@ -183,25 +256,7 @@ function Workout() {
                   : "flex justify-center items-center"
               }
             >
-              {fetchedExercies.length !== 0 ? (
-                fetchedExercies.map((ex) => {
-                  return (
-                    <div
-                      key={ex._id}
-                      className="flex justify-between w-56 bg-white py-2 px-3 rounded mb-4"
-                    >
-                      <h3 className="truncate text-ellipsis overflow-hidden">
-                        {ex.name}
-                      </h3>
-                      <button onClick={() => handleAddExerciseToWorkout(ex)}>
-                        <AddIcon />
-                      </button>
-                    </div>
-                  );
-                })
-              ) : (
-                <p className="my-6 text-slate-500 text-sm">0 results</p>
-              )}
+              {renderModalExercises()}
             </div>
             <span className="mt-3 text-xs">
               Don't see it?{" "}
