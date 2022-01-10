9525e4b37bb10d776fcf06fdd939ee1ea5fa7ddb
Jake Joseph <jakejosephcs@gmail.com>
2022-01-10 18:12:05 -0500

    add return statement to response

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -5,7 +5,7 @@ const authToken = async (req, res, next) => {
   const token = req.header("x-auth-token");
 
   if (!token) {
-    res.status(401).json({ error: "Token is missing" });
+    return res.status(401).json({ error: "Token is missing" });
   }
 
   try {
