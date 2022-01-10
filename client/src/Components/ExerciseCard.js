4485d2884c80ad7d051169a408e219820f5a2bf5
Jake Joseph <jakejosephcs@gmail.com>
2022-01-10 18:27:58 -0500

    Remove key property

--- client/src/Components/ExerciseCard.js
+++ client/src/Components/ExerciseCard.js
@@ -2,10 +2,7 @@ import DeleteIcon from "../Assests/DeleteIcon";
 
 function ExerciseCard({ exercise, handleDeleteExercise }) {
   return (
-    <div
-      key={exercise._id}
-      className="flex justify-between w-56 bg-slate-200 py-2 px-3 rounded mb-4"
-    >
+    <div className="flex justify-between w-56 bg-slate-200 py-2 px-3 rounded mb-4">
       <h3 className="truncate text-ellipsis overflow-hidden">
         {exercise.name}
       </h3>
