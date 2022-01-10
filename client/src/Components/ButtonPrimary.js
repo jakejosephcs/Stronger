97c6774081f25454d20c8cad9a5c457a6ada9def
Jake Joseph <jakejosephcs@gmail.com>
2022-01-10 18:27:29 -0500

    Modify tailwind color logic

--- client/src/Components/ButtonPrimary.js
+++ client/src/Components/ButtonPrimary.js
@@ -3,8 +3,8 @@ import React from "react";
 function ButtonPrimary({ color, onClick, text }) {
   return (
     <button
-      className={`bg-${
-        color ? color : "blue-500"
+      className={`${
+        color ? color : "bg-blue-500"
       } text-white py-2 px-5 rounded-full text-center mb-4`}
       onClick={onClick}
     >
