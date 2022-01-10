47fd36d968db5683ffe76fef7384fe8b8f45e619
Jake Joseph <jakejosephcs@gmail.com>
2022-01-10 18:32:52 -0500

    Remove routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Registration from "./Pages/Registration";
-import Login from "./Pages/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
