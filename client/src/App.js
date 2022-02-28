c5cace8f7d27c19c8214271d90adbe599c9e90ac
Jake Joseph <jakejosephcs@gmail.com>
2022-02-28 18:08:00 -0500

    Remove unused exercise context

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,20 @@ import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Workout from "./Pages/Workout";
 import Landing from "./Pages/Landing";
-import { ExericseContextProvider } from "./Context/exerciseContext";
 
 function App() {
   return (
-    <ExericseContextProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/exercises" element={<Exercises />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/workout" element={<Workout />} />
-        </Routes>
-      </BrowserRouter>
-    </ExericseContextProvider>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/exercises" element={<Exercises />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/workout" element={<Workout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
